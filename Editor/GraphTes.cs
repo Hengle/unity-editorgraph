be7a53a77fb00b736843d80cb60228effcca3de0
zouhangte <zouhantezbm@126.com>
2018-02-07 17:47:54 +0800

    开始进行窗口缩放测试

--- Editor/GraphTes.cs
+++ Editor/GraphTes.cs
@@ -59,6 +59,7 @@ public class GraphWindow : EditorWindow
     GraphGUI graphGUI;
     Node node;
     private float zoom = 1;
+    private Vector2 scrPos;
     [MenuItem("Test/Graph")]
     static void Main()
     {
@@ -76,20 +77,44 @@ public class GraphWindow : EditorWindow
     }
     private void OnGUI()
     {
+        //if (graphGUI != null)
+        //{
+        //    graphGUI.BeginToolbarGUI(new Rect(0, 0, position.width, EditorGUIUtility.singleLineHeight));
 
-        if (graphGUI != null)
+        //    zoom = GUILayout.HorizontalSlider(zoom, 0, 10);
+        //    graphGUI.drawSelectionRectCallback += CallBack;
+        //    Debug.Log(graphGUI.zoomLevel);
+        //    graphGUI.EndToolbarGUI();
+        //    graphGUI.BeginGraphGUI(this, new Rect(0, EditorGUIUtility.singleLineHeight, position.width, position.height));
+        //    graphGUI.NodeGUI(node);
+        //    graphGUI.EndGraphGUI();
+        //}
+        DrawScrollView();
+    }
+    void DrawScrollView()
+    {
+        zoom = GUILayout.HorizontalSlider(zoom, 0.5f, 2);
+        using (var scrollScope = new EditorGUILayout.ScrollViewScope(scrPos))
         {
+            scrPos = scrollScope.scrollPosition;
+            var graphRegion = GUILayoutUtility.GetRect(new GUIContent(string.Empty), GUIStyle.none, GUILayout.Width(600), GUILayout.Height(600));
+            var viewRect = new Rect(0, 0, 120/zoom, 120);
 
-            graphGUI.BeginToolbarGUI(new Rect(0, 0, position.width, EditorGUIUtility.singleLineHeight));
-            //GUILayout.Label("lable");
-            zoom = GUILayout.HorizontalSlider(zoom, 0, 10);
-            graphGUI.drawSelectionRectCallback += CallBack;
-            Debug.Log(graphGUI.zoomLevel);
-            graphGUI.EndToolbarGUI();
-            graphGUI.BeginGraphGUI(this, new Rect(0, EditorGUIUtility.singleLineHeight, position.width, position.height));
-            graphGUI.NodeGUI(node);
-            graphGUI.EndGraphGUI();
+            GUIUtility.ScaleAroundPivot(Vector2.one * zoom, -scrPos);
+            GUI.BeginClip(viewRect);
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    var rect = new Rect(i * 100, j * 100, 100, 100);
+                    GUI.Button(rect, i + ":" + j);
+                }
+            }
+            GUI.EndClip();
+            GUI.matrix = Matrix4x4.identity;
         }
+        var rect0 = GUILayoutUtility.GetRect(0, 100);
+        EditorGUI.LabelField(rect0, "Label");
     }
 
     private void CallBack(Rect selectionRect)
